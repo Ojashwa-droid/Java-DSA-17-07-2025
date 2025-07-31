053f728213cb918fbde382f12609718de9609b5e
Ojashwa Tripathi <ojashwatiwari2038@gmail.com>
2025-07-31 21:51:34 +0530

    Just a revision

--- src/com/ojas/dsa/searching/binary/algo/BinarySearchIn2DMatrix.java
+++ src/com/ojas/dsa/searching/binary/algo/BinarySearchIn2DMatrix.java
@@ -17,35 +17,62 @@ public class BinarySearchIn2DMatrix {
 
     }
 
-    public static int[] binarySearchInMatrix(int[][] matrix, int target) {
+    /**
+     * public static int[] binarySearchInMatrix(int[][] matrix, int target) {
+     * <p>
+     * // Lower bound will be the first row i.e. row 0 and upper bound will be the last column i.e. matrix.length - 1
+     * int lower = 0;
+     * int upper = matrix[0].length - 1;
+     * <p>
+     * while (lower <= upper) {
+     * <p>
+     * // First condition if element == target --> ans found
+     * if (matrix[lower][upper] == target) {
+     * return new int[]{lower, upper};
+     * }
+     * <p>
+     * // Second condition, if element < target
+     * // That means that in that particular row the elements before that element will also be smaller than the target
+     * // as the row is sorted (given) so we can ignore that row and reduce our search  space
+     * else if (matrix[lower][upper] < target) {
+     * lower++;
+     * }
+     * <p>
+     * // Third condition, if the element > target
+     * // then the rest of the element in that column will be > than the target as the column is sorted
+     * // so ignore that column as well
+     * else if (matrix[lower][upper] > target) {
+     * upper--;
+     * }
+     * }
+     * <p>
+     * // if the element is not found just return {-1,-1}
+     * return new int[]{-1, -1};
+     * }
+     */
 
-        // Lower bound will be the first row i.e. row 0 and upper bound will be the last column i.e. matrix.length - 1
+    public static int[] binarySearchInMatrix(int[][] matrix, int target) {
         int lower = 0;
         int upper = matrix[0].length - 1;
 
-        while (lower <= upper) {
-
-            // First condition if element == target --> ans found
+        // So our current element will be at indices [lower][upper]
+        // We then compare the current element with our target element
+        while (lower <= upper){
             if (matrix[lower][upper] == target) {
+                // we found our answer
                 return new int[]{lower, upper};
-            }
-
-            // Second condition, if element < target
-            // That means that in that particular row the elements before that element will also be smaller than the target
-            // as the row is sorted (given) so we can ignore that row and reduce our search  space
-            else if (matrix[lower][upper] < target) {
-                lower++;
-            }
-
-            // Third condition, if the element > target
-            // then the rest of the element in that column will be > than the target as the column is sorted
-            // so ignore that column as well
-            else if (matrix[lower][upper] > target) {
+            } else if (matrix[lower][upper] > target) {
+                // meaning that column can not have our target element
+                // so skip that column entirely
                 upper--;
+            } else if (matrix[lower][upper] < target) {
+                // That row can not have our element
+                // skip that row entirely
+                lower++;
             }
         }
 
-        // if the element is not found just return {-1,-1}
+        // In the end if the target is not found
         return new int[]{-1, -1};
     }
 }
