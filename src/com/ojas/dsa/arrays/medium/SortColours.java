82f744d61ffe5f4fe70039974580782b12036543
Ojashwa Tripathi <ojashwatiwari2038@gmail.com>
2026-01-12 16:05:21 +0530

    Striver Arrays Easy Problems

--- src/com/ojas/dsa/arrays/medium/SortColours.java
+++ src/com/ojas/dsa/arrays/medium/SortColours.java
@@ -9,18 +9,46 @@ public class SortColours {
         int[] nums = {2, 1, 2, 1, 1, 0};
 //        sortColors(nums);
 //        System.out.println(Arrays.toString(nums));
-        sortColorsV2(nums);
+//        sortColorsV2(nums);
+        sortColorsV3(nums);
         System.out.println(Arrays.toString(nums));
     }
 
 
-    public static void sortColorsV2(int[] arr){
+    // Dutch National Flag Algorithm
+    public static void sortColorsV3(int[] arr) {
+        int left = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while (mid <= high){
+            // Case 1: if zero at mid index
+            if (arr[mid] == 0){
+                swap(arr, mid, left);
+                left++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int firstIndex, int secondIndex){
+        int temp = arr[secondIndex];
+        arr[secondIndex] = arr[firstIndex];
+        arr[firstIndex] = temp;
+    }
+
+    public static void sortColorsV2(int[] arr) {
         int countZero = 0;
         int countOne = 0;
         int countTwo = 0;
 
-        for (int element: arr){
-            if (element == 0){
+        for (int element : arr) {
+            if (element == 0) {
                 countZero++;
             } else if (element == 1) {
                 countOne++;
@@ -30,7 +58,7 @@ public class SortColours {
         }
 
 
-        for (int i = 0; i < arr.length; i++){
+/*        for (int i = 0; i < arr.length; i++){
             if (countZero > 0){
                 arr[i] = 0;
                 countZero--;
@@ -40,34 +68,20 @@ public class SortColours {
             } else {
                 arr[i] = 2;
             }
+}*/
+
+        for (int i = 0; i < countZero; i++) {
+            arr[i] = 0;
         }
+
+        for (int i = countZero; i < countOne + countOne; i++) {
+            arr[i] = 1;
+        }
+
+        for (int i = countOne; i < arr.length; i++) {
+            arr[i] = 2;
+        }
+
     }
 
-
-    public static void sortColors(int[] nums) {
-        Map<Integer, Integer> seen = new HashMap<>();
-
-        for (int element : nums) {
-            if (seen.containsKey(element)) {
-                int oldFreq = seen.get(element);
-                seen.put(element, oldFreq + 1);
-            } else {
-                seen.put(element, 1);
-            }
-        }
-
-        int lastIndex = 0;
-        for (int i = 0; i <= 2; i++) {
-            int count = seen.get(i);
-            lastIndex = fillColors(nums, count, i, lastIndex);
-        }
-    }
-
-    private static int fillColors(int[] nums, int count, int colourToFill, int lastIndex) {
-        int i;
-        for (i = lastIndex; i < lastIndex + count; i++) {
-            nums[i] = colourToFill;
-        }
-        return i;
-    }
 }
